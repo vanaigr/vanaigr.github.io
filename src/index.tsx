a10b33d6dbf0bc4833f0c6d7eedc8cb21cb2a725
vanaigr <vanaigranov@gmail.com>
2025-06-01 15:31:01 -0500

    feat: setup

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import R from 'react'
+import RD from 'react-dom/client'
+
+const root = RD.createRoot(document.getElementById('root')!)
+root.render(<div>hi</div>)
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import path from 'node:path'
+
+const orig = import.meta.dirname
+const root = path.join(orig, 'src')
+
+export default defineConfig(() => {
+    return {
+        root: root,
+        publicDir: path.join(root, 'public'),
+        resolve: {
+            alias: {
+                '@/*': root,
+            }
+        }
+    }
+})
