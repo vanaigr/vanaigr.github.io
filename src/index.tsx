2ef4d55ed25dd193bee28203a0c0d6c70b1acbd3
vanaigr <vanaigranov@gmail.com>
2026-02-19 18:20:43 -0600

    @media (max-width: 75rem) { .dialogContainer > .content }

--- src/index.tsx
+++ src/index.tsx
@@ -499,7 +499,7 @@ function Dialog({ close, isOpen, it, backgroundId }: DialogProps) {
             <div
                 className={
                     s.content
-                        + ' relative w-[max(60rem,66%)] max-w-[calc(100%-2rem)] grow my-4 rounded-[1rem] overflow-hidden bg-white flex flex-col'
+                        + ' relative w-[max(60rem,66%)] max-[75rem]:w-[40rem] max-w-[calc(100%-2rem)] grow my-4 rounded-[1rem] overflow-hidden bg-white flex flex-col'
                         + ' ' + (it.videoVertical ? s.vertical + ' flex-row' : '')
                         + ' ' + s.itemAnimation
                 }
