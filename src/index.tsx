fe5935e76cece2a449649c3e23b6f869b8916bbe
vanaigr <vanaigranov@gmail.com>
2026-02-19 18:13:31 -0600

    12

--- src/index.tsx
+++ src/index.tsx
@@ -490,7 +490,7 @@ function Dialog({ close, isOpen, it, backgroundId }: DialogProps) {
             onClick={close}
             style={{ position: 'absolute', inset: '0px', cursor: 'pointer' }}
         />
-        <div className={s.closeButton}>
+        <div className={s.closeButton + ' pointer-events-none absolute right-0 top-0 py-4 px-12'}>
             <button type='button' className={'text-left ' + s.closeIcon}>
                 <CloseIcon/>
             </button>
