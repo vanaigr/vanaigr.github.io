ef73b466e3bbeb44df106e08be31317b72b8aa59
vanaigr <vanaigranov@gmail.com>
2026-02-19 20:50:21 -0600

    .externalItem > .copy > * > .icon

--- src/linkList/index.tsx
+++ src/linkList/index.tsx
@@ -66,7 +66,7 @@ function Item({ it }: { it: Link }) {
         >
             <div className='flex-1 px-2 flex items-center justify-center text-(--muted) opacity-50 transition-[transform,color,opacity] duration-100'>
                 <FontAwesomeIcon
-                    className={s.icon}
+                    className={s.icon + ' h-4'}
                     icon={faCopy}
                 />
             </div>
